485d86a6bf0bbfea258fdcf5f9387fd9e224a849
Matthias Loibl <mail@matthiasloibl.com>
2021-06-12 00:27:23 +0200

    Improve AlertsTable with color and reordering

--- ui/src/components/AlertsTable.tsx
+++ ui/src/components/AlertsTable.tsx
@@ -26,31 +26,59 @@ const AlertsTable = ({ objective }: AlertsTableProps): JSX.Element => {
       <thead>
       <tr>
         <th>Severity</th>
-        <th>Windows</th>
         <th>For</th>
         <th>Threshold</th>
-        <th>Current</th>
+        <th>Short</th>
+        <th>Long</th>
         <th>State</th>
       </tr>
       </thead>
       <tbody>
-      {alerts.map((a: MultiBurnrateAlert, i: number) => (
-        <tr key={i}>
-          <td>{a.severity}</td>
-          <td>{formatDuration(a._short.window)} and {formatDuration(a._long.window)}</td>
-          <td>{formatDuration(a._for)}</td>
-          <td>
+      {alerts.map((a: MultiBurnrateAlert, i: number) => {
+        let shortCurrent = '';
+        if (a._short.current === -1.0) {
+          shortCurrent = 'NaN'
+        } else if (a._short.current === undefined) {
+          shortCurrent = (0).toFixed(3).toString()
+        } else {
+          shortCurrent = a._short.current.toFixed(3)
+        }
+        let longCurrent = '';
+        if (a._long.current === -1.0) {
+          longCurrent = 'NaN'
+        } else if (a._long.current === undefined) {
+          longCurrent = (0).toFixed(3).toString()
+        } else {
+          longCurrent = a._long.current.toFixed(3)
+        }
+
+        let stateColor = '#1B5E20'
+        if (a.state === 'firing') {
+          stateColor = '#B71C1C'
+        }
+        if (a.state === 'pending') {
+          stateColor = '#F57F17'
+        }
+
+        return (
+          <tr key={i}>
+            <td>{a.severity}</td>
+            <td>{formatDuration(a._for)}</td>
+            <td>
             <span title={`${a.factor} * (1 - ${objective?.target})`}>
               {(a.factor * (1 - objective?.target)).toFixed(3)}
             </span>
-          </td>
-          <td>
-            {a._short.current !== undefined ? a._short.current.toFixed(3) : (0).toFixed(3)}&nbsp;
-            {a._long.current !== undefined ? a._long.current.toFixed(3) : (0).toFixed(3)}
-          </td>
-          <td>{a.state}</td>
-        </tr>
-      ))}
+            </td>
+            <td>
+              {shortCurrent} ({formatDuration(a._short.window)})
+            </td>
+            <td>
+              {longCurrent} ({formatDuration(a._long.window)})
+            </td>
+            <td><span style={{ color: stateColor }}>{a.state}</span></td>
+          </tr>
+        )
+      })}
       </tbody>
     </Table>
   )
