030f2f3ec69045f60a5baca0bc11d09ba972d112
Matthias Loibl <mail@matthiasloibl.com>
2021-07-21 00:04:51 +0200

    Separate colors into importable file

---
+++ ui/src/components/colors.tsx
@@ -0,0 +1,44 @@
+export const greens = [
+  '1B5E20',
+  '2E7D32',
+  '388E3C',
+  '43A047',
+  '4CAF50',
+  '66BB6A',
+  '81C784',
+  'A5D6A7',
+  'C8E6C9'
+]
+export const blues = [
+  "0D47A1",
+  "1565C0",
+  "1976D2",
+  "1E88E5",
+  "2196F3",
+  "42A5F5",
+  "64B5F6",
+  "90CAF9",
+  "BBDEFB"
+]
+export const reds = [
+  "B71C1C",
+  "C62828",
+  "D32F2F",
+  "E53935",
+  "F44336",
+  "EF5350",
+  "E57373",
+  "EF9A9A",
+  "FFCDD2"
+]
+export const yellows = [
+  "F57F17",
+  "F9A825",
+  "FBC02D",
+  "FDD835",
+  "FFEB3B",
+  "FFEE58",
+  "FFF176",
+  "FFF59D",
+  "FFF9C4"
+]
