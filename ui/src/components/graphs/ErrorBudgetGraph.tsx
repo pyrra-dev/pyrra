9c28612e1e515d94a0e96ea4952a72436634e5e7
Matthias Loibl <mail@matthiasloibl.com>
2021-12-07 02:07:14 +0100

    ui: Use uplot-react for graphs going forward
    A couple of TODOs are left, but it's looking really good

--- ui/src/components/graphs/ErrorBudgetGraph.tsx
+++ ui/src/components/graphs/ErrorBudgetGraph.tsx
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { Spinner } from 'react-bootstrap'
-import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, TooltipProps, XAxis, YAxis } from 'recharts'
+import UplotReact from 'uplot-react';
+import uPlot, { AlignedData } from 'uplot'
 
-import { dateFormatter, dateFormatterFull, formatDuration, PROMETHEUS_URL } from '../../App'
+import { formatDuration, PROMETHEUS_URL } from '../../App'
 import { ObjectivesApi, QueryRange } from '../../client'
 import { IconExternal } from '../Icons'
-import { greens, reds } from '../colors'
+import { greens } from '../colors'
 import { labelsString } from "../../labels";
 
 interface ErrorBudgetGraphProps {
-    api: ObjectivesApi
-    labels: { [key: string]: string }
-    grouping: { [key: string]: string }
-    timeRange: number
+  api: ObjectivesApi
+  labels: { [key: string]: string }
+  grouping: { [key: string]: string }
+  timeRange: number
 }
 
 const ErrorBudgetGraph = ({ api, labels, grouping, timeRange }: ErrorBudgetGraphProps): JSX.Element => {
-  const [samples, setSamples] = useState<any[]>([]);
+  const [samples, setSamples] = useState<AlignedData>();
+  // TODO: Are these even needed with uplot going forward?
   const [samplesOffset, setSamplesOffset] = useState<number>(0)
   const [samplesMin, setSamplesMin] = useState<number>(-10000)
   const [samplesMax, setSamplesMax] = useState<number>(1)
   const [query, setQuery] = useState<string>('')
   const [loading, setLoading] = useState<boolean>(true)
+  const [start, setStart] = useState<number>()
+  const [end, setEnd] = useState<number>()
 
   useEffect(() => {
     setLoading(true)
@@ -32,84 +36,68 @@ const ErrorBudgetGraph = ({ api, labels, grouping, timeRange }: ErrorBudgetGraph
 
     api.getObjectiveErrorBudget({ expr: labelsString(labels), grouping: labelsString(grouping), start, end })
       .then((r: QueryRange) => {
-        let data: any[] = []
-        r.values.forEach((v: number[], i: number) => {
-          v.forEach((v: number, j: number) => {
-            if (j === 0) {
-              data[i] = { t: v }
-            } else {
-              data[i].v = v
-            }
-          })
-        })
-
-        const minRaw = Math.min(...data.map((o) => o.v))
-        const maxRaw = Math.max(...data.map((o) => o.v))
-        const diff = maxRaw - minRaw
-
-        let roundBy = 1
-        if (diff < 1) {
-          roundBy = 10
-        }
-        if (diff < 0.1) {
-          roundBy = 100
-        }
-        if (diff < 0.01) {
-          roundBy = 1_000
-        }
-
-        // Calculate the offset to split the error budget into green and red areas
-        const min = Math.floor(minRaw * roundBy) / roundBy;
-        const max = Math.ceil(maxRaw * roundBy) / roundBy;
-
-        setSamplesMin(min === 1 ? 0 : min)
-        setSamplesMax(max)
-        if (max <= 0) {
-          setSamplesOffset(0)
-        } else if (min >= 1) {
-          setSamplesOffset(1)
-        } else {
-          setSamplesOffset(maxRaw / (maxRaw - minRaw))
-        }
-        setSamples(data)
+        // let data: any[] = []
+        // r.values.forEach((v: number[], i: number) => {
+        //   v.forEach((v: number, j: number) => {
+        //     if (j === 0) {
+        //       data[i] = { t: v }
+        //     } else {
+        //       data[i].v = v
+        //     }
+        //   })
+        // })
+        //
+        // const minRaw = Math.min(...data.map((o) => o.v))
+        // const maxRaw = Math.max(...data.map((o) => o.v))
+        // const diff = maxRaw - minRaw
+        //
+        // let roundBy = 1
+        // if (diff < 1) {
+        //   roundBy = 10
+        // }
+        // if (diff < 0.1) {
+        //   roundBy = 100
+        // }
+        // if (diff < 0.01) {
+        //   roundBy = 1_000
+        // }
+        //
+        // // Calculate the offset to split the error budget into green and red areas
+        // const min = Math.floor(minRaw * roundBy) / roundBy;
+        // const max = Math.ceil(maxRaw * roundBy) / roundBy;
+        //
+        // setSamplesMin(min === 1 ? 0 : min)
+        // setSamplesMax(max)
+        // if (max <= 0) {
+        //   setSamplesOffset(0)
+        // } else if (min >= 1) {
+        //   setSamplesOffset(1)
+        // } else {
+        //   setSamplesOffset(maxRaw / (maxRaw - minRaw))
+        // }
+        setSamples([
+          r.values[0],
+          r.values[1].map((v: number) => 100 * v)
+        ])
         setQuery(r.query)
+        setStart(start)
+        setEnd(end)
       })
       .finally(() => setLoading(false))
   }, [api, labels, grouping, timeRange])
 
-  if (!loading && samples.length === 0) {
+  if (!loading && samples === undefined) {
     return <>
       <h4>Error Budget</h4>
       <div><p>What percentage of the error budget is left over time?</p></div>
     </>
   }
-
-  const DateTooltip = ({ payload }: TooltipProps<number, number>): JSX.Element => {
-    const style = {
-      padding: 10,
-      paddingTop: 5,
-      paddingBottom: 5,
-      backgroundColor: 'white',
-      border: '1px solid #666',
-      borderRadius: 3
-    }
-    if (payload !== undefined && payload?.length > 0) {
-      return (
-        <div className="area-chart-tooltip" style={style}>
-          Date: {dateFormatterFull(payload[0].payload.t)}<br/>
-          Value: {(100 * payload[0].payload.v).toFixed(3)}%
-        </div>
-      )
-    }
-    return <></>
-  }
-
   return (
     <>
       <div style={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
         <h4>
           Error Budget
-          {loading && samples.length !== 0 ? (
+          {loading ? (
             <Spinner animation="border" style={{
               marginLeft: '1rem',
               marginBottom: '0.5rem',
@@ -129,52 +117,43 @@ const ErrorBudgetGraph = ({ api, labels, grouping, timeRange }: ErrorBudgetGraph
           </a>
         ) : <></>}
       </div>
-      <div><p>What percentage of the error budget is left over time?</p></div>
-      {loading && samples.length === 0 ?
-        <div style={{
-          width: '100%',
-          height: 230,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-          <Spinner animation="border" style={{ margin: '0 auto' }}/>
-        </div>
-        : <ResponsiveContainer height={300}>
-          <AreaChart height={300} data={samples}>
-            <XAxis
-              type="number"
-              dataKey="t"
-              tickCount={7}
-              tickFormatter={dateFormatter(timeRange)}
-              domain={[samples[0].t, samples[samples.length - 1].t]}
-            />
-            <YAxis
-              width={70}
-              tickCount={5}
-              unit="%"
-              tickFormatter={(v: number) => (100 * v).toFixed(2)}
-              domain={[samplesMin, samplesMax]}
-            />
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Tooltip content={<DateTooltip/>}/>
-            <defs>
-              <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-                <stop offset={samplesOffset} stopColor={`#${greens[0]}`} stopOpacity={1}/>
-                <stop offset={samplesOffset} stopColor={`#${reds[0]}`} stopOpacity={1}/>
-              </linearGradient>
-            </defs>
-            <Area
-              dataKey="v"
-              type="monotone"
-              connectNulls={false}
-              animationDuration={250}
-              strokeWidth={0}
-              fill="url(#splitColor)"
-              fillOpacity={1}/>
-          </AreaChart>
-        </ResponsiveContainer>
-      }
+      <div>
+        <p>What percentage of the error budget is left over time?</p>
+      </div>
+
+      {samples !== undefined ? (
+        <UplotReact options={{
+          width: 1000,
+          height: 300,
+          padding: [20, 0, 0, 20],
+          series: [{}, {
+            stroke: `#${greens[0]}`,
+            fill: `#${greens[0]}`
+          }],
+          scales: {
+            x: { min: start, max: end },
+            y: {
+              range: {
+                min: {},
+                max: { hard: 100 }
+              }
+            }
+          },
+          axes: [{}, {
+            values: (uplot: uPlot, v: number[]) => (v.map((v: number) => `${v.toFixed(2)}%`))
+          }]
+        }} data={samples}/>
+      ) : (
+        <UplotReact options={{
+          width: 1000,
+          height: 300,
+          series: [{}, {}],
+          scales: {
+            x: { min: start, max: end },
+            y: { min: 0, max: 1 }
+          }
+        }} data={[[], []]}/>
+      )}
     </>
   )
 }
--- ui/src/components/graphs/RequestsGraph.tsx
+++ ui/src/components/graphs/RequestsGraph.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Spinner } from 'react-bootstrap'
-import { TooltipProps } from 'recharts'
+import UplotReact from 'uplot-react';
+import uPlot, { AlignedData } from 'uplot'
 
 import { ObjectivesApi, QueryRange } from '../../client'
-import { dateFormatterFull, formatDuration, PROMETHEUS_URL } from '../../App'
+import { formatDuration, PROMETHEUS_URL } from '../../App'
 import { IconExternal } from '../Icons'
 import { labelsString } from "../../labels";
-import uPlot, { AlignedData } from 'uplot'
+import { greens } from '../colors'
 
 interface RequestsGraphProps {
   api: ObjectivesApi
@@ -18,8 +19,11 @@ interface RequestsGraphProps {
 const RequestsGraph = ({ api, labels, grouping, timeRange }: RequestsGraphProps): JSX.Element => {
   const [requests, setRequests] = useState<AlignedData>()
   const [requestsQuery, setRequestsQuery] = useState<string>('')
+  // TODO: Add support for various labels again
   const [requestsLabels, setRequestsLabels] = useState<string[]>([])
   const [requestsLoading, setRequestsLoading] = useState<boolean>(true)
+  const [start, setStart] = useState<number>()
+  const [end, setEnd] = useState<number>()
 
   useEffect(() => {
     const now = Date.now()
@@ -29,51 +33,18 @@ const RequestsGraph = ({ api, labels, grouping, timeRange }: RequestsGraphProps)
     setRequestsLoading(true)
     api.getREDRequests({ expr: labelsString(labels), grouping: labelsString(grouping), start, end })
       .then((r: QueryRange) => {
-        // let data: any[] = []
-        // r.values.forEach((v: number[], i: number) => {
-        //   v.forEach((v: number, j: number) => {
-        //     if (j === 0) {
-        //       data[i] = { t: v }
-        //     } else {
-        //       data[i][j - 1] = v
-        //     }
-        //   })
-        // })
         let data: AlignedData = [
           r.values[0],
-          r.values[1],
+          r.values[1]
         ]
 
         setRequestsLabels(r.labels)
         setRequestsQuery(r.query)
         setRequests(data)
+        setStart(start)
+        setEnd(end)
       }).finally(() => setRequestsLoading(false))
   }, [api, labels, grouping, timeRange])
-
-  const RequestTooltip = ({ payload }: TooltipProps<number, number>): JSX.Element => {
-    const style = {
-      padding: 10,
-      paddingTop: 5,
-      paddingBottom: 5,
-      backgroundColor: 'white',
-      border: '1px solid #666',
-      borderRadius: 3
-    }
-    if (payload !== undefined && payload?.length > 0) {
-      return (
-        <div className="area-chart-tooltip" style={style}>
-          Date: {dateFormatterFull(payload[0].payload.t)}<br/>
-          {Object.keys(payload[0].payload).filter((k) => k !== 't').map((k: string, i: number) => (
-            <div key={i}>
-              {requestsLabels[i] !== '{}' ? `${requestsLabels[i]}:` : ''}
-              {(payload[0].payload[k]).toFixed(2)} req/s
-            </div>
-          ))}
-        </div>
-      )
-    }
-    return <></>
-  }
 
   return (
     <>
@@ -103,31 +74,48 @@ const RequestsGraph = ({ api, labels, grouping, timeRange }: RequestsGraphProps)
       </div>
 
       {requests !== undefined ? (
-        <Plot opts={{ width: 500, height: 150, series: [{}, { stroke: 'red', label: '200' }] }}
-              data={requests}/>
+        <UplotReact options={{
+          width: 500,
+          height: 150,
+          series: [{}, {
+            stroke: `#${greens[0]}`,
+            label: '200', // TODO: Use actual label
+            gaps: (u: uPlot, seriesID: number, startIdx: number, endIdx: number): uPlot.Series.Gaps => {
+              let delta = 5 * 60
+              let xData = u.data[0]
+
+              let gaps: uPlot.Series.Gaps = []
+              for (let i = startIdx + 1; i <= endIdx; i++) {
+                if (xData[i] - xData[i - 1] > delta) {
+                  uPlot.addGap(
+                    gaps,
+                    Math.round(u.valToPos(xData[i - 1], 'x', true)),
+                    Math.round(u.valToPos(xData[i], 'x', true))
+                  );
+                }
+              }
+              return gaps
+            }
+          }],
+          scales: {
+            x: { min: start, max: end },
+            y: {
+              range: {
+                min: { hard: 0 },
+                max: {}
+              }
+            }
+          }
+        }} data={requests}/>
       ) : (
-        <></>
+        <UplotReact options={{
+          width: 500,
+          height: 150,
+          series: [{}, {}],
+          scales: { x: {}, y: { min: 0, max: 1 } }
+        }} data={[[], []]}/>
       )}
     </>
-  )
-}
-
-interface PlotProps {
-  opts: uPlot.Options
-  data: uPlot.AlignedData
-}
-
-const Plot = ({ opts, data }: PlotProps): JSX.Element => {
-  const uPlotRef = React.createRef<HTMLDivElement>()
-
-  useEffect(() => {
-    if (uPlotRef.current !== null) {
-      new uPlot(opts, data, uPlotRef.current)
-    }
-  }, [uPlotRef, opts, data])
-
-  return (
-    <div ref={uPlotRef}/>
   )
 }
 
