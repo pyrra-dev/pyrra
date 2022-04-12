3feae359ca6fd83d423dc83e04a681e7c39ebd04
Matthias Loibl <mail@matthiasloibl.com>
2022-04-12 15:48:05 +0200

    ui: Delete unused files

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.scss'
 import "@fontsource/lato";
 import "@fontsource/inter";
@@ -12,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
