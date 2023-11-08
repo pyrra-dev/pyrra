95795b27b15e493bf74456844fae11690e122dc2
Juraj Michalek <juraj.michalek@matillion.com>
2023-11-08 11:23:07 +0100

    chore: update faling unit tests

--- slo/rules_test.go
+++ slo/rules_test.go
@@ -1749,7 +1749,7 @@ func TestObjective_GrafanaRules(t *testing.T) {
 				Labels: map[string]string{"slo": "monitoring-grpc-latency"},
 			}, {
 				Record: "pyrra_errors_total",
-				Expr:   intstr.FromString(`sum(rate(grpc_server_handling_seconds_count{grpc_method="Write",grpc_service="conprof.WritableProfileStore",job="api"})[5m]) - sum(rate(grpc_server_handling_seconds_bucket{grpc_method="Write",grpc_service="conprof.WritableProfileStore",job="api",le="0.6"}[5m))`),
+				Expr:   intstr.FromString(`sum(rate(grpc_server_handling_seconds_count{grpc_method="Write",grpc_service="conprof.WritableProfileStore",job="api"}[5m])) - sum(rate(grpc_server_handling_seconds_bucket{grpc_method="Write",grpc_service="conprof.WritableProfileStore",job="api",le="0.6"}[5m))`),
 				Labels: map[string]string{"slo": "monitoring-grpc-latency"},
 			}},
 		},
